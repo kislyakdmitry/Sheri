be8b6bb620216f5374cb7d699d8bdc7126ee10d3
Dmitry Kislyak <kislyakdmitry@gmail.com>
2019-01-07 00:39:36 +0300

    Create

---
+++ src/main/java/ru/sheri/ShopApplication.java
@@ -0,0 +1,11 @@
+package ru.sheri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApplication {
+    public static void main(String[] Args){
+        SpringApplication.run(ShopApplication.class);
+    }
+}
