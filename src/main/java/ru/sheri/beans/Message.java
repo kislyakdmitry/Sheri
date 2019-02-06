f089febc83e429dd777d02f63526012b2dc2110e
Dmitry Kislyak <kislyakdmitry@gmail.com>
2019-02-06 20:15:25 +0300

    db connect

---
+++ src/main/java/ru/sheri/beans/Message.java
@@ -0,0 +1,44 @@
+package ru.sheri.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String text;
+
+    private String tag;
+
+    Message(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
